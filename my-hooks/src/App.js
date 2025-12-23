2ff4376dd74fb0c6934e4346468f684adecdaa53
Стефан <bukotko2009@gmail.com>
2025-12-23 12:05:26 +0300

    commit

--- my-hooks/src/App.js
+++ my-hooks/src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
-import Counter from './Components/Counter';
 import FeedbackWidget from './Components/Feedback';
 
 function App() {
   return (
     <div className="App">
       <FeedbackWidget />
-      <Counter />
     </div>
   );
 }
